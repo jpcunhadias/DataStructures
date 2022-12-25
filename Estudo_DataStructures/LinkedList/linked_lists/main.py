8aa170657881194ae9dd109c8b01eae5c114c0a7
jpcunhadias <joaopedrocdias@hotmail.com>
2022-12-25 17:58:31 -0300

    single_linked_lists complete

---
+++ Estudo_DataStructures/LinkedList/linked_lists/main.py
@@ -0,0 +1,5 @@
+from src.single_linked_list.single_ll import SingleLinkedList
+
+test_ll = SingleLinkedList()
+
+test_ll.reverse_list()
---
+++ Estudo_DataStructures/LinkedList/linked_lists/tests/test_functions_sll.py
@@ -0,0 +1,5 @@
+from linked_lists.src.single_linked_list.single_ll import SingleLinkedList
+
+test_list = SingleLinkedList()
+
+test_list.print_list()
