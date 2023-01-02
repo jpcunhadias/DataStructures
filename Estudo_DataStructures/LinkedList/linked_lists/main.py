e8cb891108763926aec64b6c8a6036f397feb0b2
João Pedro da Cunha Dias <joaopedrocdias@hotmail.com>
2023-01-01 23:22:54 -0300

    fixing branch

--- Estudo_DataStructures/LinkedList/linked_lists/main.py
+++
@@ -1,5 +0,0 @@
-from src.single_linked_list.single_ll import SingleLinkedList
-
-test_ll = SingleLinkedList()
-
-test_ll.reverse_list()
--- Estudo_DataStructures/LinkedList/linked_lists/nodes.py
+++
@@ -1,5 +0,0 @@
-class Node:
-    def __init__(self, value, prev=None, next=None):
-        self.value = value
-        self.prev = prev
-        self.next = next
